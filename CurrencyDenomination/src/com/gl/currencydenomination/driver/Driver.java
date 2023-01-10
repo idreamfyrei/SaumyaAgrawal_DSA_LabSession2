ada9af73e95cb48c2d29f99d540628f90de9ff57
idreamfyrei <118106463+idreamfyrei@users.noreply.github.com>
2023-01-10 07:30:58 +0530

    Add files via upload

---
+++ CurrencyDenomination/src/com/gl/currencydenomination/driver/Driver.java
@@ -0,0 +1,30 @@
+package com.gl.currencydenomination.driver;
+
+import java.util.Scanner;
+
+import com.gl.denominationcalc.transaction.DenominationCalculator;
+
+public class Driver {
+	public static void main(String[] args) {
+	       
+        System.out.println("Enter the total number of currency denominations");
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+        int[] denominations = new int[size];
+        System.out.println("Enter the denominations");
+        for (int i = 0; i < size; i++) {
+            denominations[i] = input.nextInt();
+        }
+        System.out.println("Enter the amount to be paid");
+        int amount = input.nextInt();
+       
+        DenominationCalculator calculator = new DenominationCalculator(denominations, amount);
+        calculator.calculate();
+       
+        if(input !=null) {
+            input.close();
+        }
+                       
+    }
+
+}
